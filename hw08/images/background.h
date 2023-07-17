e7f321029e048edaab5b2aaa14b1eccea0e80125
Phan, Eric Hoang <ephan6916@gmail.com>
2023-07-17 11:36:48 -0400

    cleaned up files and 100/100 tl1

---
+++ hw08/images/background.h
@@ -0,0 +1,23 @@
+/*
+ * Exported with nin10kit v1.8
+ * Invocation command was nin10kit --mode=3 --resize=10x10 background background.png 
+ * Time-stamp: Sunday 07/16/2023, 18:40:00
+ * 
+ * Image Information
+ * -----------------
+ * background.png 10@10
+ * 
+ * All bug reports / feature requests are to be filed here https://github.com/TricksterGuy/nin10kit/issues
+ */
+
+#ifndef BACKGROUND_H
+#define BACKGROUND_H
+
+extern const unsigned short background[100];
+#define BACKGROUND_SIZE 200
+#define BACKGROUND_LENGTH 100
+#define BACKGROUND_WIDTH 10
+#define BACKGROUND_HEIGHT 10
+
+#endif
+
